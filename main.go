ea644e213d366f5839bd0cfb3d8010de515daf66
Bobae Kang <bkang.dev@gmail.com>
2019-12-05 09:19:54 -0600

    add extra routes

--- main.go
+++ main.go
@@ -11,10 +11,20 @@ import (
 func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
-	
+	router.GET("/arrests", ArrestsIndex)
+	router.GET("/arrests/by-offense-class", ArrestsByOffenseClassPath)
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintln(w, "Hello, World!")
+}
+
+func ArrestsIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintln(w, "Arrests all")
+}
+
+func ArrestsByOffenseClassPath(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintln(w, "Arrests by offense class")
 }
