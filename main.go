b6b8299941f90a20f9c8dcf58566749b9da222d3
Bobae Kang <bkang.dev@gmail.com>
2019-12-05 10:08:00 -0600

    try models using dummy data

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -23,8 +24,22 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 func ArrestsIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintln(w, "Arrests all")
+	data := ArrestsAll{
+		ArrestsAllOne{Year: 2017, Value: 1820},
+		ArrestsAllOne{Year: 2018, Value: 1795},
+	}
+	json.NewEncoder(w).Encode(data)
 }
 
 func ArrestsByOffenseClassPath(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintln(w, "Arrests by offense class")
+	data := ArrestsByOffenseClass{
+		ArrestsByOffenseClassOne{Year: 2017, OffenseClass: 0, Value: 162},
+		ArrestsByOffenseClassOne{Year: 2017, OffenseClass: 1, Value: 1277},
+		ArrestsByOffenseClassOne{Year: 2017, OffenseClass: 2, Value: 81},
+		ArrestsByOffenseClassOne{Year: 2018, OffenseClass: 0, Value: 421},
+		ArrestsByOffenseClassOne{Year: 2018, OffenseClass: 1, Value: 1253},
+		ArrestsByOffenseClassOne{Year: 2018, OffenseClass: 2, Value: 121},
+	}
+	json.NewEncoder(w).Encode(data)
 }
