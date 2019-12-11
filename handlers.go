f564912cd7385133656a5b88c8480e6ab113787d
Bobae Kang <bkang.dev@gmail.com>
2019-12-11 12:13:56 -0600

    put sample arrests data into a file

--- handlers.go
+++ handlers.go
@@ -13,23 +13,11 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 }
 
 func ArrestsIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	data := []ArrestsAll{
-		ArrestsAll{Year: 2017, Value: 1820},
-		ArrestsAll{Year: 2018, Value: 1795},
-	}
-	writeOKResponse(w, data)
+	writeOKResponse(w, SampleDataArrestsAll)
 }
 
 func ArrestsByOffenseClassPath(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	data := []ArrestsByOffenseClass{
-		ArrestsByOffenseClass{Year: 2017, OffenseClass: 0, Value: 162},
-		ArrestsByOffenseClass{Year: 2017, OffenseClass: 1, Value: 1277},
-		ArrestsByOffenseClass{Year: 2017, OffenseClass: 2, Value: 81},
-		ArrestsByOffenseClass{Year: 2018, OffenseClass: 0, Value: 421},
-		ArrestsByOffenseClass{Year: 2018, OffenseClass: 1, Value: 1253},
-		ArrestsByOffenseClass{Year: 2018, OffenseClass: 2, Value: 121},
-	}
-	writeOKResponse(w, data)
+	writeOKResponse(w, SampleDataArrestsByOffenseClass)
 }
 
 func writeOKResponse(w http.ResponseWriter, m interface{}) {
---
+++ sample_arrests.go
@@ -0,0 +1,45 @@
+package main
+
+var SampleDataArrestsAll = []ArrestsAll{
+	{
+		Year:  2017,
+		Value: 1820,
+	},
+	{
+		Year:  2018,
+		Value: 1795,
+	},
+}
+
+var SampleDataArrestsByOffenseClass = []ArrestsByOffenseClass{
+	{
+		Year:         2017,
+		OffenseClass: 0,
+		Value:        162,
+	},
+	{
+		Year:         2017,
+		OffenseClass: 1,
+		Value:        1277,
+	},
+	{
+		Year:         2017,
+		OffenseClass: 2,
+		Value:        81,
+	},
+	{
+		Year:         2018,
+		OffenseClass: 0,
+		Value:        421,
+	},
+	{
+		Year:         2018,
+		OffenseClass: 1,
+		Value:        1253,
+	},
+	{
+		Year:         2018,
+		OffenseClass: 2,
+		Value:        121,
+	},
+}
