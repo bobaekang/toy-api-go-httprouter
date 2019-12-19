27b06fe026dd2bf4a6621e927d482f4936b3710e
Bobae Kang <bkang.dev@gmail.com>
2019-12-19 17:11:30 -0600

    add doc comments

--- arrests/model.go
+++ arrests/model.go
@@ -1,10 +1,12 @@
 package arrests
 
+// All models a row in Arrests table.
 type All struct {
 	Year  int `json:"year"`
 	Value int `json:"value"`
 }
 
+// ByOffenseClass models a row in ArrestsByOffenseClass table.
 type ByOffenseClass struct {
 	Year         int `json:"year"`
 	OffenseClass int `json:"offenseclass"`
--- arrests/service.go
+++ arrests/service.go
@@ -1,10 +1,12 @@
 package arrests
 
+// Repository provides access to arrests data storage.
 type Repository interface {
 	GetArrestsAll() []All
 	GetArrestsByOffenseClass() []ByOffenseClass
 }
 
+// Service provides operations for arrests data.
 type Service interface {
 	GetArrestsAll() []All
 	GetArrestsByOffenseClass() []ByOffenseClass
@@ -14,14 +16,17 @@ type service struct {
 	r Repository
 }
 
+// NewService creates a arrests service with the necessary dependencies.
 func NewService(r Repository) Service {
 	return &service{r}
 }
 
+// GetArrestsAll returns arrests data.
 func (s *service) GetArrestsAll() []All {
 	return s.r.GetArrestsAll()
 }
 
+// GetArrestsAll returns arrests by offense class data.
 func (s *service) GetArrestsByOffenseClass() []ByOffenseClass {
 	return s.r.GetArrestsByOffenseClass()
 }
